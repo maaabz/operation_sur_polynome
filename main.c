8282816f685ec69fd78c909335ba87663d18f0ea
maaabz <86616157+maaabz@users.noreply.github.com>
2025-11-30 20:26:15 +0000

    mise à jour du main.c

--- main.c
+++ main.c
@@ -1,2 +1,52 @@
 #include <stdio.h>
 
+int entreepolynome(int *degre, int coeffs[]); //prend en entree le degré du polynomes et ses coeffs
+int sommepolynome(int degre1, int coeffs1[], int degre2, int coeffs2[], int resultat[]);
+
+
+int main(){
+
+int degre1;
+int poly1[100];
+int degre2;
+int poly2[100];
+
+entreepolynome(&degre1,poly1);
+
+return 0;
+
+
+}
+
+int entreepolynome(int *degre, int coeffs[]){
+
+printf("Quelle est le degré de votre polynome (entier naturel) ? \n");
+scanf("%d",degre);
+printf("Quelle sont ses coefficiants ? (Par ordre croissant a_0,a_1...a_degre)\n");
+for(int i = 0; i<= *degre; i++) {
+    printf("a_%d = ",i);
+    int a;
+    scanf("%d",&a);
+    coeffs[i] = a;
+
+
+}
+
+printf("Vous avez saisi le polynome suivant : \n");
+for(int i = 0;i<=*degre;i++){ printf("a_%d = %d \n",i,coeffs[i]); }
+
+
+
+}
+
+
+int sommepolynome(int degre1, int coeffs1[], int degre2, int coeffs2[], int resultat[]){
+int degremax;
+if(degre1<degre2){ degremax = degre2};
+else{degremax = degre1;}
+
+for 
+
+}
+
+
