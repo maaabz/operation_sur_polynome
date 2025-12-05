73e141e8be04444dfb42bcc99444726b8e84d68b
maaabz <86616157+maaabz@users.noreply.github.com>
2025-12-05 09:38:57 +0000

    Version finale

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 
 // Le code se compile avec : gcc main.c -o main -lm
 
+void ecrire(const char *message);
+void polynome_vers_texte(int degre, int coeffs[], char *buffer);
 void entreepolynome(int *degre, int coeffs[]);
 void afficherpolynome(int *degre, int coeffs[]);
 void sommepolynome(int *degre1, int coeffs1[], int *degre2, int coeffs2[]);
@@ -20,6 +22,32 @@ void devlimite(int *degre1, int coeffs1[], float a, int n);   // Développement 
 void methodenewton(int *degre1, int coeffs1[], float a, float b, int n);
 
 
+void ecrire(const char *message) 
+{
+    FILE *fichier = fopen("journal.log", "a");
+    if (fichier != NULL) {
+        // On ajoute un timestamp simple si tu veux, ou juste le message
+        fprintf(fichier, "%s\n", message); 
+        fclose(fichier);
+    } else {
+        printf("Erreur d'accès au fichier log.\n");
+    }
+}
+
+void polynome_vers_texte(int degre, int coeffs[], char *buffer) 
+{
+    buffer[0] = '\0'; 
+    char terme[50];   
+
+    for(int i = 0; i <= degre; i++) {
+        if (i == 0) {
+            sprintf(terme, "%d", coeffs[i]);
+        } else {
+            sprintf(terme, "%+dX^%d", coeffs[i], i);
+        }
+        strcat(buffer, terme);
+    }
+}
 
 int main(){
 
@@ -51,16 +79,16 @@ while(!quitter) {printf("\n ===== OPERATIONS SUR POLYNOMES ===== \n");
         
         case 3 : entreepolynome(&degre1,poly1) ; deriveepolynome(&degre1, poly1) ; break;
 
-        case 4 : printf("\n-----SAISIT DU POLYNOME-----\n") ;entreepolynome(&degre1,poly1) ;
+        case 4 : {printf("\n-----SAISIT DU POLYNOME-----\n") ;entreepolynome(&degre1,poly1) ;
         float a; float b; printf("\nQuelle est le segment d'intégration [a;b] ?\n");
-        printf("Choisissez a :\n"); scanf("%f",&a) ;  printf("Choisissez b :\n") ; scanf("%f",&b) ; integraleintervalle(&degre1, poly1, a,b); break;
+        printf("Choisissez a :\n"); scanf("%f",&a) ;  printf("Choisissez b :\n") ; scanf("%f",&b) ; integraleintervalle(&degre1, poly1, a,b); break;}
         
-        case 5 : printf("\n-----SAISIT DU POLYNOME-----\n"); entreepolynome(&degre1,poly1) ; printf("Pour votre DL_N(a), que vaut n et a ?\n"); int nn ; float aa; 
-        printf("Choisissez a :\n"); scanf("%f",&aa) ;  printf("Choisissez n :\n") ; scanf("%d",&nn) ; devlimite(&degre1, poly1, aa, nn) ; break;
+        case 5 :{ printf("\n-----SAISIT DU POLYNOME-----\n"); entreepolynome(&degre1,poly1) ; printf("Pour votre DL_N(a), que vaut n et a ?\n"); int nn ; float aa; 
+        printf("Choisissez a :\n"); scanf("%f",&aa) ;  printf("Choisissez n :\n") ; scanf("%d",&nn) ; devlimite(&degre1, poly1, aa, nn) ; break;}
 
-        case 6 : printf("\n-----SAISIT DU POLYNOME-----\n") ; entreepolynome(&degre1,poly1) ; printf("Vous souhaitez trover la/les racine(s) eventuelle(s) du polynomes saisit sur un intervalle [a;b]. \n");
+        case 6 :{ printf("\n-----SAISIT DU POLYNOME-----\n") ; entreepolynome(&degre1,poly1) ; printf("Vous souhaitez trouver une racine eventuelle du polynome saisit sur un intervalle [a;b]. \n");
         float a1; float b1; printf("Choisissez a :\n"); scanf("%f",&a1) ;  printf("Choisissez b :\n") ; scanf("%f",&b1) ; methodenewton(&degre1, poly1,a1,b1,100000) ;
-        break;
+        break;}
 
         case 7 : quitter = true ; printf("\nCiao\n");
     }
@@ -70,8 +98,6 @@ while(!quitter) {printf("\n ===== OPERATIONS SUR POLYNOMES ===== \n");
 return 0;
 
 }
-
-
 
 void entreepolynome(int *degre, int coeffs[]){
 
@@ -102,78 +128,118 @@ for(int i = 0;i<=*degre;i++){ printf("a_%d = %d \n",i,coeffs[i]); }
 
 void sommepolynome(int *degre1, int coeffs1[], int *degre2, int coeffs2[]){
     
-int a = 0; // si a est nulle, deg(poly1)<=deg(poly2)
+	int a = 0; // si a est nulle, deg(poly1)<=deg(poly2)
 
-int degremax;
-if(*degre1<*degre2){ degremax = *degre2;}
-else{degremax = *degre1; a = 1;}
-
-
-int degremin;
-if(*degre1<*degre2){ degremin = *degre1;}
-else{degremin = *degre2;}
-
-int somme[degremax+1];
-for(int i = 0; i<=degremin; i++) { somme[i] = coeffs1[i]+coeffs2[i];}
-
-if(a==0){
-for(int i = degremin + 1; i<=degremax ; i++) {somme[i] = coeffs2[i];} }
+	int degremax;
+	if(*degre1<*degre2){ degremax = *degre2;}
+	else{degremax = *degre1; a = 1;}
 
 
-if(a==1){
-for(int i = degremin + 1; i<=degremax ; i++) {somme[i] = coeffs1[i];} }
+	int degremin;
+	if(*degre1<*degre2){ degremin = *degre1;}
+	else{degremin = *degre2;}
 
-printf("La somme de ces deux polynomes est : \n ");
-afficherpolynome(&degremax,somme);
+	int somme[degremax+1];
+	for(int i = 0; i<=degremin; i++) { somme[i] = coeffs1[i]+coeffs2[i];}
 
+	if(a==0){
+	for(int i = degremin + 1; i<=degremax ; i++) {somme[i] = coeffs2[i];} }
+
+
+	if(a==1){
+	for(int i = degremin + 1; i<=degremax ; i++) {somme[i] = coeffs1[i];} }
+
+	printf("La somme de ces deux polynomes est : \n ");
+	afficherpolynome(&degremax,somme);
+	
+	char p1s[1024], p2s[1024], res_s[1024];
+	
+	polynome_vers_texte(*degre1, coeffs1, p1s);
+    polynome_vers_texte(*degre2, coeffs2, p2s);
+    polynome_vers_texte(degremax, somme, res_s);
+    	
+    int taille = snprintf(NULL, 0, "Somme : (%s) + (%s) = (%s)", p1s, p2s, res_s) + 1;
+    char *msg = malloc(taille);
+    if (msg != NULL) {
+		snprintf(msg, taille, "Somme : (%s) + (%s) = (%s)", p1s, p2s, res_s);
+		ecrire(msg);
+		free(msg);
+	    }
 
 }
 
 
 void produitpolynome(int *degre1, int coeffs1[], int *degre2, int coeffs2[]){
 
-int a = 0; // si a est nulle, deg(poly1)<=deg(poly2)
+	int a = 0; // si a est nulle, deg(poly1)<=deg(poly2)
 
-int degrepoly = *degre1 + *degre2;
+	int degrepoly = *degre1 + *degre2;
 
 
-int premier[degrepoly+1];
-int deuxieme[degrepoly+1];
+	int premier[degrepoly+1];
+	int deuxieme[degrepoly+1];
 
-for(int k = 0 ; k <= *degre1 ; k ++) {premier[k]=coeffs1[k];}
-for(int k = 0 ; k <= *degre2 ; k ++) {deuxieme[k]=coeffs2[k];}
-for(int k = *degre1 + 1  ; k <= degrepoly ; k ++) {premier[k]=0;}
-for(int k = *degre2 + 1  ; k <= degrepoly ; k ++) {deuxieme[k]=0;}
+	for(int k = 0 ; k <= *degre1 ; k ++) {premier[k]=coeffs1[k];}
+	for(int k = 0 ; k <= *degre2 ; k ++) {deuxieme[k]=coeffs2[k];}
+	for(int k = *degre1 + 1  ; k <= degrepoly ; k ++) {premier[k]=0;}
+	for(int k = *degre2 + 1  ; k <= degrepoly ; k ++) {deuxieme[k]=0;}
 
-int produit[degrepoly+1];
+	int produit[degrepoly+1];
 
-for(int k = 0 ; k <= degrepoly ; k ++) {
-    int res = 0;
-    for(int p = 0; p<=k ; p++) {
-        res += premier[p]*deuxieme[k-p];
+	for(int k = 0 ; k <= degrepoly ; k ++) {
+	    int res = 0;
+	    for(int p = 0; p<=k ; p++) {
+		res += premier[p]*deuxieme[k-p];
 
-}
-produit[k]=res; 
-}
+	}
+	produit[k]=res; 
+	}
 
-printf("Le produit de ces deux polynomes est : \n ");
-afficherpolynome(&degrepoly,produit);
+	printf("Le produit de ces deux polynomes est : \n ");
+	afficherpolynome(&degrepoly,produit);
+	
+	char p1s[1024], p2s[1024], res_s[2048];
 
+	polynome_vers_texte(*degre1, coeffs1, p1s);
+    polynome_vers_texte(*degre2, coeffs2, p2s);
+    polynome_vers_texte(degrepoly, produit, res_s);
+    	
+    int taille = snprintf(NULL, 0, "Produit : (%s) * (%s) = (%s)", p1s, p2s, res_s) + 1;
+    char *msg = malloc(taille);
+    
+	if (msg != NULL) {
+		snprintf(msg, taille, "Produit : (%s) * (%s) = (%s)", p1s, p2s, res_s);
+		ecrire(msg);
+		free(msg);
+	    }
 
 }
 
 
 void deriveepolynome(int *degre1, int coeffs1[]){
 
-if(*degre1==0){printf("La dérivée de ce polynome est le polynome nulle \n"); return ;}
+	if(*degre1==0){printf("La dérivée de ce polynome est le polynome nulle \n"); return ;}
 
-int degrederive = *degre1 - 1;
-int derive[*degre1];
-for(int k = 0 ; k <= degrederive ; k ++) {derive[k]=coeffs1[k+1]*(k+1);}
+	int degrederive = *degre1 - 1;
+	int derive[*degre1];
+	for(int k = 0 ; k <= degrederive ; k ++) {derive[k]=coeffs1[k+1]*(k+1);}
 
-printf("La derivée du polynome saisie est : \n ");
-afficherpolynome(&(degrederive),derive);
-return;
+	printf("La derivée du polynome saisie est : \n ");
+	afficherpolynome(&(degrederive),derive);
+
+	char p1s[1024], p2s[1024];
+
+	polynome_vers_texte(*degre1, coeffs1, p1s);      
+	polynome_vers_texte(degrederive, derive, p2s);
+	
+	int taille = snprintf(NULL, 0, "Derivation effectue : ( %s)' = ( %s)", p1s, p2s) + 1;
+	char *msg = malloc(taille);
+	
+	if (msg != NULL) {
+	       snprintf(msg, taille, "Dérivation effectue : ( %s)' = ( %s)", p1s, p2s);
+	       ecrire(msg);
+	       free (msg);
+	}
 }
 
 void calculer_derivee(int degree, int coeffse[], int *degres, int coeffss[]) {
@@ -194,69 +260,125 @@ void calculer_derivee(int degree, int coeffse[], int *degres, int coeffss[]) {
     
 void integraleintervalle(int *degre1, int coeffs1[], float a, float b){
 
-float FA = 0.0;
-float FB = 0.0;
-for(int k = 0 ; k <= *degre1 ; k ++) {FA+=((float)coeffs1[k]*pow(a,k+1))/(k+1);}
-for(int k = 0 ; k <= *degre1 ; k ++) {FB+=((float)coeffs1[k]*pow(b,k+1))/(k+1);}
-float integraleab = FB-FA;
-printf("L'intégrale de %f à %f du polynome vaut %f \n",a,b, integraleab);
+	float FA = 0.0;
+	float FB = 0.0;
+	for(int k = 0 ; k <= *degre1 ; k ++) {FA+=((float)coeffs1[k]*pow(a,k+1))/(k+1);}
+	for(int k = 0 ; k <= *degre1 ; k ++) {FB+=((float)coeffs1[k]*pow(b,k+1))/(k+1);}
+	float integraleab = FB-FA;
+	printf("L'intégrale de %f à %f du polynome vaut %f \n",a,b, integraleab);
+	
+	char p1s[1024];
+	
+	polynome_vers_texte(*degre1, coeffs1, p1s);
+	
+	int taille = snprintf(NULL, 0, "Integrale de (%s) sur [%.2f ; %.2f] = %f", p1s, a, b, integraleab) + 1;
+	
+	char *msg = malloc(taille);
+    if (msg != NULL) {
+        	snprintf(msg, taille, "Integrale de (%s) sur [%.2f ; %.2f] = %f", p1s, a, b, integraleab);
+        	ecrire(msg);
+        	free(msg);
+	}
 }
-
 
 float valeurpoint(float a , int coeffs[], int *degre1){
+	
+	float res = 0;
+	for(int i = 0; i<=*degre1 ; i++) { res+= coeffs[i]*pow(a,i);}
+	return res;
+    }
 
-float res = 0;
-for(int i = 0; i<=*degre1 ; i++) { res+= coeffs[i]*pow(a,i);}
-return res;
-}
 
 
 
 void devlimite(int *degre1, int coeffs1[], float a, int n){
 
-int aderiver[*degre1];
-for(int i = 0 ; i<= *degre1 ; i++) {aderiver[i]=coeffs1[i];}
+    int aderiver[200];
+    for(int i = 0 ; i<= *degre1 ; i++) aderiver[i]=coeffs1[i];
+    int deg = *degre1;
 
-int deg = *degre1;
-for(int i = 0; i<=n ; i++){
-    float b = valeurpoint(a, aderiver, &deg);
-    int fact = 1;
-    for(int j = 1; j <= i; j++) fact *= j;
-    printf("%f*(X-a)^%d \n", b/fact, i);
+    printf("DL_%d(a) = ", n);
 
-    
-    int deg2;
-    int derive2[100];
-    calculer_derivee(deg, aderiver, &deg2, derive2);
-    deg = deg2;
-    for(int k=0; k<=deg2; k++) aderiver[k] = derive2[k];
+    for(int k = 0; k <= n; k++){
+
+        float bk = valeurpoint(a, aderiver, &deg);
+
+        int fact = 1;
+        for(int j = 1; j <= k; j++) fact *= j;
+
+        float coeff = bk / fact;
+
+        if (k == 0) printf("%f", coeff);
+        else printf(" + %f*(X-%.3f)^%d", coeff, a, k);
+
+        int deg2;
+        int derive2[200];
+        calculer_derivee(deg, aderiver, &deg2, derive2);
+
+        deg = deg2;
+        for(int t=0; t<=deg2; t++) aderiver[t] = derive2[t];
+    }
+
+    printf("\n");
+
+    char p1s[1024];
+    polynome_vers_texte(*degre1, coeffs1, p1s);
+
+    int taille = snprintf(NULL, 0, "Calcul DL ordre %d en a=%f pour (%s)", n, a, p1s) + 1;
+    char *msg = malloc(taille);
+    if (msg != NULL) {
+        snprintf(msg, taille, "Calcul DL ordre %d en a=%f pour (%s)", n, a, p1s);
+        ecrire(msg);
+        free(msg);
+    }
 }
 
-}
 
 
 
 void methodenewton(int *degre1, int coeffs1[], float a, float b, int n){
 
-float xo = (a+b)/2;
-float eps = 0.00001; // il s'agit de notre précision
+	float xo = (a+b)/2;
+	float eps = 0.00001; // il s'agit de notre précision
+	
+	char p1s[1024];
+	polynome_vers_texte(*degre1, coeffs1, p1s);
+	
+	for(int i = 0 ; i<=n ; i++){
+	    float px = valeurpoint(xo,coeffs1,degre1);
 
-for(int i = 0 ; i<=n ; i++){
-    float px = valeurpoint(xo,coeffs1,degre1);
+	    int degre_derive;
+	    int coeffsderive[100];   
+	    calculer_derivee(*degre1, coeffs1, &degre_derive, coeffsderive);
+	    float dpx = valeurpoint(xo, coeffsderive, &degre_derive);
+	    
+	    if(fabs(dpx) < 0.00000001) { printf("Division par 0 impossible (dérivée nulle)\n"); 
+	    ecrire("Echec Newton : Derivee nulle (division par 0)");
+	    return;}
 
-    int degre_derive;
-    int coeffsderive[100];   
-    calculer_derivee(*degre1, coeffs1, &degre_derive, coeffsderive);
-    float dpx = valeurpoint(xo, coeffsderive, &degre_derive);
-    if(fabs(dpx) < 0.00000001) { printf("Division par 0 impossible (dérivée nulle)\n"); return;}
+	    float nouvx = xo - px/dpx;
+	    if (fabs(nouvx - xo)<eps) {printf("Une racine (presque exact) de P est %f \n", nouvx);
+	    
+	    int taille = snprintf(NULL, 0, "Newton succes : Racine = %f pour polynome (%s)", nouvx, p1s) + 1;
+         char *msg = malloc(taille);
+         if (msg != NULL) {
+                snprintf(msg, taille, "Newton succes : Racine = %f pour polynome (%s)", nouvx, p1s);
+                ecrire(msg);
+                free(msg);
+                }      
+	     return;
+	     }
+	    xo = nouvx;
 
-    float nouvx = xo - px/dpx;
-    if (fabs(nouvx - xo)<eps) {printf("Une racine (presque exact) de P est %f \n", nouvx); return;}
+	}
 
-    xo = nouvx;
+	printf("Aucune racine n'a pu etre approximée dans cet intervalle. \n");
 
-}
-
-printf("Aucune racine n'a pu etre approximée dans cet intervalle. \n");
-
+	int taille = snprintf(NULL, 0, "Newton echec : Pas de convergence apres %d iterations pour (%s)", n, p1s) + 1;
+    char *msg = malloc(taille);
+    if (msg != NULL) {
+        snprintf(msg, taille, "Newton echec : Pas de convergence apres %d iterations pour (%s)", n, p1s);
+        ecrire(msg);
+        free(msg);
+	}
 }
